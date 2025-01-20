c384263ecf0170b594b84d7fd0e0444971c9e955
ubska <alessandraubska@gmail.com>
2025-01-20 16:05:28 +0100

    feat: add api key

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 const cityInput = document.querySelector('.city-input');
 // select search btn
 const searchBtn = document.querySelector('.search-btn');
+// API key
+const apiKey = '992e740f0c6c102df1eead968076d548';
 
 searchBtn.addEventListener('click', () => {
     if (cityInput.value.trim() != '') {
-        updateWeatherInfo()
+        updateWeatherInfo(cityInput.value)
         cityInput.value = '';
         cityInput.blur()
     }
@@ -15,9 +17,18 @@ cityInput.addEventListener('keydown', (event) => {
     if (event.key == 'Enter' &&
         cityInput.value.trim() != ''
     ) {
-        updateWeatherInfo()
+        updateWeatherInfo(cityInput.value)
         cityInput.value = '';
         cityInput.blur()
     }
     
 })
+
+
+function getFatchData() {
+    
+}
+
+function updateWeatherInfo(city) {
+    const weatherData = getFatchData()
+}
