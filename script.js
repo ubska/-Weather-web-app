991d3eb9f5b7d9e7dd49cc346e8a11f5410c334d
ubska <alessandraubska@gmail.com>
2025-01-20 17:19:46 +0100

    feat: add update weather

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const searchBtn = document.querySelector('.search-btn');
 // API key
 const apiKey = '992e740f0c6c102df1eead968076d548';
 
+const weatherInfoSection = document.querySelector('.weather-info');
+const notFoundSection = document.querySelector('.not-found');
+const searchCitySection = document.querySelector('.search-city');
+
 searchBtn.addEventListener('click', () => {
     if (cityInput.value.trim() != '') {
         updateWeatherInfo(cityInput.value)
@@ -26,13 +30,27 @@ cityInput.addEventListener('keydown', (event) => {
 
 
 async function getFatchData(endPoint, city) {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`;
 
     const response = await fetch(apiUrl);
     return response.json();
 }
 
 async function updateWeatherInfo(city) {
-    const weatherData = await getFatchData('weather', 'city');
-    console.log(weatherData);
+    const weatherData = await getFatchData('weather', city);
+
+    if (weatherData.cod != 200) {
+        showDisplaySection(notFoundSection)
+        return 
+    }
+
+    showDisplaySection(weatherInfoSection)
+
+}
+
+function showDisplaySection(section) {
+    [weatherInfoSection, searchCitySection, notFoundSection]
+        .forEach(section => section.style.display = 'none')
+    
+    section.style.display = 'flex'
 }
