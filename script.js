52a3e073bb7b953a39c96073bec652c8714411b6
ubska <alessandraubska@gmail.com>
2025-01-20 15:59:51 +0100

    feat: add eventlistener search btn and city input

--- script.js
+++ script.js
@@ -2,3 +2,22 @@
 const cityInput = document.querySelector('.city-input');
 // select search btn
 const searchBtn = document.querySelector('.search-btn');
+
+searchBtn.addEventListener('click', () => {
+    if (cityInput.value.trim() != '') {
+        updateWeatherInfo()
+        cityInput.value = '';
+        cityInput.blur()
+    }
+});
+
+cityInput.addEventListener('keydown', (event) => {
+    if (event.key == 'Enter' &&
+        cityInput.value.trim() != ''
+    ) {
+        updateWeatherInfo()
+        cityInput.value = '';
+        cityInput.blur()
+    }
+    
+})
